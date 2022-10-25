a0c32eece0f334d27016d9318a42aaef86ffe708
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2022-10-25 22:07:26 +0600

    initialize firebase and config firebase and implement singin and signup functionalities

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8E4NQvdaBaXlOWRTRN_T9_3fmX-B4YtM",
+    authDomain: "study-care-12dd0.firebaseapp.com",
+    projectId: "study-care-12dd0",
+    storageBucket: "study-care-12dd0.appspot.com",
+    messagingSenderId: "662053417807",
+    appId: "1:662053417807:web:046660523dc4718da30b3a",
+    measurementId: "G-1ECCVX3RC7"
+  };
+
+  export default firebaseConfig
